8cff0cea4d3b6f0c0ba1b95b149dc0735b32dbb9
akash-panthri <akashpanthri2000@gmail.com>
2024-07-09 07:56:17 +0530

    Added expess server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const PORT = 3000;
+
+app.get('hello',(req, res)=>{
+    res.send('Task Manager App')
+})
+app.listen(PORT, console.log(`server listening on ${PORT}`))
