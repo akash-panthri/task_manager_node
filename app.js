214a809fbd2a2cead8b30ff0edbec050a2caca72
akash-panthri <akashpanthri2000@gmail.com>
2024-07-14 07:25:16 +0530

    Added code in not-found.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const PORT = 3000;
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
 
 
 //connect to db
@@ -20,4 +21,5 @@ app.get('/hello',(req, res)=>{
 })
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
 app.listen(PORT, console.log(`server listening on ${PORT}`))
